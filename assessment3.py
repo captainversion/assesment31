48550235e4222eef6ce816c174730f252798c233
captainversion <136670723+captainversion@users.noreply.github.com>
2023-06-29 21:56:39 +0530

    assessment3.py

--- assessment3.py
+++ assessment3.py
@@ -1,6 +1,9 @@
-def sum(numbers):
-    a= 0
-    for x in numbers:
-        a+= x
-    return a
-print(sum((1, 2, 3, 4, 5)))
+list = []
+total = 0
+num = int(input('How many numbers:'))
+for n in range(num):
+    numbers = int(input('Enter number:'))
+    list.append(numbers)
+for a in range(0, len(list)):
+    total = total + list[a]
+print("The Sum of user input value is :", total)
